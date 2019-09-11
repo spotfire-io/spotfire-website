36fd8025ebe738fcd547747d7f235959268b8852
Cedric Hurst <cedric@spantree.net>
2019-09-11 09:06:24 -0500

    updating site config for correct pwa name

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Spotfire`,
+    description: `Optimize your Spotify Playlists for Musicality`,
+    author: `@divideby0`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
